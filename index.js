fe26e329b94a3eaf732e2a07cad3829efc3f64a4
nziokikevin <kevinnzioki10@gmail.com>
2022-06-24 15:58:42 +0300

    adding an eventlistener

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ const getVerse = async () => {
 	//console.log(item)
 }
 getVerse()
+
+const form = document.querySelector('.form');
+const submitInput = form[0].querySelector('input[type="submit"]');
+
+function getDataForm(e){
+	e.preventDefault();
+	const formData = new FormData(form[0]);
+	alert(formData.get('nameField') + '-' + formData.get('emailField'));
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	submitInput.addEventListener('click', getDataForm, false);
+}, false);
